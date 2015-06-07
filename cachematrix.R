bca7b87c7f3a4f6c818fdda2cf35b58a4c6359ec
AndroidWargames <adwagner89@gmail.com>
2015-06-07 15:58:53 -0500

    Finished

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m1 <- NULL
+  set <- function(y){
+    x <<- y
+    m1 <<- NULL
+  }
+  get <- function() x
+  setinv <- function(slv) m1 <<- slv
+  getinv <- function() m1
+  list(set = set, get = get,
+   setinv = setinv,
+   getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+  m1 <- x$getinv()
+  if(!is.null(m1)) {
+    message("getting cached data")
+    return(m1)
+  }
+  data <- x$get()
+  m1 <- solve(data)
+  x$setinv(m1)
+  m1
 }
